7ceb05893ac7ba7699b28ab05f9068a10dc16cdb
Jeff <jeffrey.miiller@gmail.com>
2020-01-30 01:20:56 -0800

    Added the initial website HTML and CSS.
    Credit to Shay Howe's CSS and HTML "Learn to Code HTML and CSS". It was a fantastic resource. This website is the result of reading up to chapter 7 and applying what I have learned along the way.

--- front_page/views.py
+++ front_page/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h1>My site</h1>")
+    return render(request, 'front_page/front_page.html', {})
